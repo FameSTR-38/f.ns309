a407abc7d3a7a7717d1807286f7747eb29ca91e7
unknown <bl1247533@gmail.com>
2020-08-13 13:07:30 +0630

    BeachResort

---
+++ src/Contentful.js
@@ -0,0 +1,6 @@
+import {CreatClient, createClient} from 'contentful'
+
+export default createClient({
+    space: "tgntpegs6uwc",
+    accessToken : "ozMQf-6g3f5a1EjmW5St66cT7EBE2Iy6R5kQA8UuqWk"
+});
